c6bad26f509f3b3513936499fb114731c7229398
Priyanka Katoch <priyankakatoch@Priyankas-MacBook-Air.local>
2025-10-18 21:00:29 +0530

    Add MaxSubarraySumCSES solution using Kadane's Algorithm

---
+++ MaxSubarraySumCSES.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MaxSubarraySumCSES {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLong();
+        }
+
+        long maxSum = arr[0];
+        long currentSum = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        System.out.println(maxSum);
+        sc.close();
+    }
+}
+
